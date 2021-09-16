342d94ef3786db742ab531b5a80d94a286cd1e11
caarlosdamian <dapecarlos@gmail.com>
2021-09-15 19:36:36 -0500

    Feat/Add Schema to Server-Server Running

---
+++ server/FakeData.js
@@ -0,0 +1,39 @@
+ UserList = [
+  {
+    id: 1,
+    name: "Carlos",
+    username: "Caarlosdamian",
+    age: 25,
+    nationality: "Mexican",
+  },
+  {
+    id: 2,
+    name: "Carlos",
+    username: "Caarlosdamian",
+    age: 29,
+    nationality: "Mexican",
+  },
+  {
+    id: 3,
+    name: "Carlos",
+    username: "Caarlosdamian",
+    age: 23,
+    nationality: "Mexican",
+  },
+  {
+    id: 4,
+    name: "Carlos",
+    username: "Caarlosdamian",
+    age: 19,
+    nationality: "Mexican",
+  },
+  {
+    id: 5,
+    name: "Carlos",
+    username: "Caarlosdamian",
+    age: 24,
+    nationality: "Mexican",
+  },
+];
+
+module.exports = {UserList}
