e040119d846b19cba2749aaead256044ee9fc7f9
caarlosdamian <dapecarlos@gmail.com>
2021-09-15 20:35:39 -0500

    Feat/Modify Schema

--- server/FakeData.js
+++ server/FakeData.js
@@ -1,39 +1,91 @@
- UserList = [
+  
+const UserList = [
   {
     id: 1,
-    name: "Carlos",
-    username: "Caarlosdamian",
-    age: 25,
-    nationality: "Mexican",
+    name: "John",
+    username: "john",
+    age: 20,
+    nationality: "CANADA",
+    friends: [
+      {
+        id: 2,
+        name: "Pedro",
+        username: "PedroTech",
+        age: 20,
+        nationality: "BRAZIL",
+      },
+      {
+        id: 5,
+        name: "Kelly",
+        username: "kelly2019",
+        age: 5,
+        nationality: "CHILE",
+      },
+    ],
   },
   {
     id: 2,
-    name: "Carlos",
-    username: "Caarlosdamian",
-    age: 29,
-    nationality: "Mexican",
+    name: "Pedro",
+    username: "PedroTech",
+    age: 20,
+    nationality: "BRAZIL",
   },
   {
     id: 3,
-    name: "Carlos",
-    username: "Caarlosdamian",
-    age: 23,
-    nationality: "Mexican",
+    name: "Sarah",
+    username: "cameron",
+    age: 25,
+    nationality: "INDIA",
+    friends: [
+      {
+        id: 2,
+        name: "Pedro",
+        username: "PedroTech",
+        age: 20,
+        nationality: "BRAZIL",
+      },
+    ],
   },
   {
     id: 4,
-    name: "Carlos",
-    username: "Caarlosdamian",
-    age: 19,
-    nationality: "Mexican",
+    name: "Rafe",
+    username: "rafe123",
+    age: 60,
+    nationality: "GERMANY",
   },
   {
     id: 5,
-    name: "Carlos",
-    username: "Caarlosdamian",
-    age: 24,
-    nationality: "Mexican",
+    name: "Kelly",
+    username: "kelly2019",
+    age: 5,
+    nationality: "CHILE",
+  },
+];
+const MovieList = [
+  {
+    id: 1,
+    name: "Avengers Endgame",
+    yearOfPublication: 2019,
+    isInTheaters: true,
+  },
+  {
+    id: 2,
+    name: "Interstellar",
+    yearOfPublication: 2007,
+    isInTheaters: true,
+  },
+  {
+    id: 3,
+    name: "Superbad",
+    yearOfPublication: 2009,
+    isInTheaters: true,
+  },
+  {
+    id: 4,
+    name: "PedroTech The Movie",
+    yearOfPublication: 2035,
+    isInTheaters: false,
   },
 ];
 
-module.exports = {UserList}
+module.exports = { UserList, MovieList };
